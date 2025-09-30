06a19aa3f854c1326afa0378bbb1fd10c424422c
Ryuji <ryu1101216@gmail.com>
2025-09-30 15:30:38 +0900

    first commit

---
+++ git.bat
@@ -0,0 +1,7 @@
+echo "# WCBCafe" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/ryu1101216/WCBCafe.git
+git push -u origin main
