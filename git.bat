5f20af327bfcc6d66ef12c53c8eb697802fc520f
Ryuji <ryu1101216@gmail.com>
2025-09-30 16:16:42 +0900

    contact修正、不要ファイル削除

--- git.bat
+++
@@ -1,7 +0,0 @@
-echo "# WCBCafe" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/ryu1101216/WCBCafe.git
-git push -u origin main
